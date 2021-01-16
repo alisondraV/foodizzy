574e4d1d0f43fc15d595e1e65b26a20bcf367360
alisondraV <alice.7414122013@gmail.com>
2021-01-15 23:43:57 -0500

    Bring in Firestore

---
+++ src/Firestore.ts
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+export default class Firestore {
+    public db!: firebase.firestore.Firestore;
+
+    private static _instance: Firestore | null = null;
+
+    public static get instance(): Firestore {
+        if (this._instance === null) {
+            this._instance = new Firestore();
+        }
+        return this._instance;
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import firebase from "firebase";
+import Firestore from "@/Firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+};
+const app = firebase.initializeApp(firebaseConfig);
+Firestore.instance.db = firebase.firestore(app);
 
 Vue.config.productionTip = false;
 
