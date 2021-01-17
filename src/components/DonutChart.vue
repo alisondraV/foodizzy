733859a544030f1c7ecadfdba43cc842782413f5
ANDREYDEN <andrey2850@gmail.com>
2021-01-17 00:51:24 -0500

    Incorporate a donut chart

---
+++ src/components/DonutChart.vue
@@ -0,0 +1,44 @@
+<template>
+  <canvas id="doughnut" />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import Chart from 'chart.js'
+
+@Component
+export default class DoughnutChart extends Vue {
+  @Prop({ default: [] }) readonly labels!: Array<string>
+  @Prop({ default: [] }) readonly colors!: Array<string>
+  @Prop({ default: [] }) readonly data!: Array<number>
+  @Prop({
+    default: () => {
+      return Chart.defaults.doughnut
+    }
+  })
+  readonly options: object | undefined
+
+  mounted() {
+    this.createChart({
+      datasets: [
+        {
+          data: this.data,
+          backgroundColor: this.colors
+        }
+      ],
+      labels: this.labels
+    })
+  }
+
+  createChart(chartData: object) {
+    const canvas = document.getElementById('doughnut') as HTMLCanvasElement
+    const options = {
+      type: 'doughnut',
+      data: chartData,
+      borderWidth: 2,
+      options: this.options
+    }
+    new Chart(canvas, options)
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,11 @@
   <div>
     <v-header heading="" />
     <div class="mt-20 mb-20 mx-8">
-      <p v-for="wastedProduct in wastedProducts" :key="wastedProduct.name">
-        {{ wastedProduct.name }}
-      </p>
+      <DonutChart :data="[25, 75, 30]"
+      :labels="['Red', 'Green', 'Blue']"
+      :colors="['red', 'green', 'blue']">
+        68%
+      </DonutChart>
     </div>
     <navigation-menu />
   </div>
@@ -19,11 +21,13 @@ import WastedProduct from "@/types/WastedProduct";
 import Family from "@/types/Family";
 import VHeader from "@/components/VHeader.vue";
 import firebase from 'firebase';
+import DonutChart from '@/components/DonutChart.vue'
 
 @Component({
   components: {
     VHeader,
-    NavigationMenu
+    NavigationMenu,
+    DonutChart
   }
 })
 export default class Home extends Vue {
@@ -64,6 +68,24 @@ export default class Home extends Vue {
 
       return acc;
     }, {});
+  }
+
+  get chartProps() {
+    const totalWaste = Object.values(this.statistics).reduce((acc, e) => acc + e, 0)
+    const colors = ['green', 'blue', 'yellow', 'red', 'black']
+
+    return {
+      size: 250,
+      sections: Object.keys(this.statistics).map((category, i) => {
+        return {
+          label: category,
+          value: this.statistics[category] * 100 / totalWaste,
+          color: colors[i]
+        }
+      }),
+      thickness: 40,
+      hasLegend: true,
+    }
   }
 }
 </script>
