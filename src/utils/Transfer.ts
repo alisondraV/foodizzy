0bbefd1990eccc1545ab84e25329500e286023a2
ANDREYDEN <andrey2850@gmail.com>
2021-01-31 13:10:38 -0500

    Add recipe data

--- src/utils/Transfer.ts
+++ src/utils/Transfer.ts
@@ -2,7 +2,7 @@ import Firestore from "./Firestore";
 import docs from './data.json'
 
 export async function logCollection(name: string) {
-    const allProductsSnap = await Firestore.instance.db.collection('allProducts').get()
+    const allProductsSnap = await Firestore.instance.db.collection(name).get()
 
     const allProducts = allProductsSnap.docs.map(doc => doc.data())
 
@@ -10,7 +10,7 @@ export async function logCollection(name: string) {
 }
 
 export async function fillCollection(name: string) {
-    const allProductsCollection = await Firestore.instance.db.collection('allProducts')
+    const allProductsCollection = await Firestore.instance.db.collection(name)
 
     docs.forEach(doc => {
         allProductsCollection.add(doc)
