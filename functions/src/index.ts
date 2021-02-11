e5e961f760b4bcd34bb2caa2ffaf62e0e7e939a3
ANDREYDEN <andrey2850@gmail.com>
2021-02-11 09:20:04 -0500

    Refactor function to update totalProducts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,34 +1,41 @@
 import * as functions from "firebase-functions";
 
-export const helloWorld = functions.firestore
+export const onFamilyUpdate = functions.firestore
   .document("/family/{familyId}")
   .onUpdate((change, context) => {
-
     const oldFamily = change.before.data();
     const newFamily = change.after.data();
 
-    if (oldFamily.storage.length >= newFamily.storage.length) {
-      return
+    if (newFamily.storage.length >= oldFamily.storage.length) {
+      return updateTotalProducts(newFamily, oldFamily, change);
     }
-
-    let addedProduct: any = null
-    newFamily.storage.forEach((newProduct: any) => {
-      if (!oldFamily.storage.find((oldProduct: any) => newProduct.name === oldProduct.name)) {
-        addedProduct = newProduct
-      }
-    })
-    console.log("A new product added: ", addedProduct);
-
-    if (addedProduct == null) {
-      throw new Error("The storages were the same")
-    }
-
-    const categoryName = addedProduct!.category.toLowerCase() ?? "general"
-
-    if (!Object.keys(newFamily.totalProducts).includes(categoryName)) {
-      newFamily.totalProducts[categoryName] = 0
-    }
-    newFamily.totalProducts[categoryName]++
-
-    change.after.ref.update("totalProducts", newFamily.totalProducts)
+    
+    return null;
   });
+
+async function updateTotalProducts(
+  newFamily: FirebaseFirestore.DocumentData, 
+  oldFamily: FirebaseFirestore.DocumentData, 
+  change: functions.Change<functions.firestore.QueryDocumentSnapshot>
+) {
+  let addedProduct: any = null
+  newFamily.storage.forEach((newProduct: any) => {
+    if (!oldFamily.storage.find((oldProduct: any) => newProduct.name === oldProduct.name)) {
+      addedProduct = newProduct
+    }
+  })
+  console.log("A new product added: ", addedProduct);
+
+  if (addedProduct == null) {
+    throw new Error("The storages were the same")
+  }
+
+  const categoryName = addedProduct!.category.toLowerCase() ?? "general"
+
+  if (!Object.keys(newFamily.totalProducts).includes(categoryName)) {
+    newFamily.totalProducts[categoryName] = 0
+  }
+  newFamily.totalProducts[categoryName]++
+
+  return change.after.ref.update("totalProducts", newFamily.totalProducts)
+}
