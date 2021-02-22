3cf3ca164f18dd91839b774ec27f382b2cb4dd59
alisondraV <alice.7414122013@gmail.com>
2021-02-21 21:25:32 -0500

    Add products functionality

--- src/views/FridgeSetup.vue
+++ src/views/FridgeSetup.vue
@@ -1,26 +1,28 @@
 <template>
-  <div>
-    <div class="mt-12 mb-20 mx-8">
-      <h1 class="mb-4 w-4/5 text-header font-extrabold text-primary-text">
-        What is in your fridge?
-      </h1>
-      <search-input class="mb-4" v-model="searchQuery" />
-      <div
-        class="mb-4"
-        v-for="category in Object.keys(filteredCategoryProducts)"
-        :key="category"
-      >
-        <h2 class="text-primary-text text-lg mb-1">{{ category }}</h2>
-        <div class="flex flex-wrap -mx-2">
-          <div
-            class="bg-light-grey rounded py-2 px-3 mx-2 my-1"
-            style="width: 45%"
-            v-for="product in filteredCategoryProducts[category]"
-            :key="product.name"
-            :product="product"
-          >
-            {{ product.name }}
-          </div>
+  <div class="mt-12 mb-12 mx-8">
+    <h1 class="mb-4 w-4/5 text-header font-extrabold text-primary-text">
+      What is in your fridge?
+    </h1>
+    <search-input class="mb-4" v-model="searchQuery" />
+    <div
+      class="mb-4"
+      v-for="category in Object.keys(filteredCategoryProducts)"
+      :key="category"
+    >
+      <h2 class="text-primary-text text-lg mb-1">{{ category }}</h2>
+      <div class="flex flex-wrap -mx-2">
+        <div
+          class="rounded py-2 px-3 mx-2 my-1"
+          :class="
+            isInProductsList(product) ? 'bg-light-yellow' : 'bg-light-grey'
+          "
+          style="width: 45%"
+          v-for="product in filteredCategoryProducts[category]"
+          :key="product.name"
+          :product="product"
+          @click="addToProductList(product)"
+        >
+          {{ product.name }}
         </div>
       </div>
     </div>
@@ -40,9 +42,8 @@ import SearchInput from "@/components/SearchInput.vue";
 })
 export default class Fridge extends Vue {
   products: Product[] = [];
+  productsToAdd: Product[] = [];
   searchQuery = "";
-  newProductName = "";
-  newProductCategory = "";
 
   async mounted() {
     this.products = await this.getProductsWithCategory();
@@ -54,6 +55,19 @@ export default class Fridge extends Vue {
       const productCategory = product.category ?? "General";
       return { name: product.name, category: productCategory };
     });
+  }
+
+  addToProductList(product: Product) {
+    if (this.productsToAdd.includes(product)) {
+      return (this.productsToAdd = this.productsToAdd.filter(
+        prevProduct => prevProduct != product
+      ));
+    }
+    this.productsToAdd.push(product);
+  }
+
+  isInProductsList(product: Product) {
+    return this.productsToAdd.includes(product);
   }
 
   get filteredCategoryProducts() {
