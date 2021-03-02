6283ff766c448ebb988dea852657c9c2d109a482
alisondraV <alice.7414122013@gmail.com>
2021-03-01 23:44:56 -0500

    Add image to CustomAlert

--- src/components/VAlert.vue
+++ src/components/VAlert.vue
@@ -1,8 +1,14 @@
 <template>
   <div
-    class="w-full bg-light-peach text-primary-text text-lg font-bold h-14 p-3 pl-6"
+    class="flex w-full bg-light-peach text-primary-text text-lg font-bold h-14 p-3 pl-6"
   >
     {{ label }}
+    <img
+      class="ml-3"
+      v-if="wasted"
+      src="@/assets/images/SadFace.svg"
+      alt="Wasted"
+    />
   </div>
 </template>
 
@@ -12,5 +18,6 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class VInput extends Vue {
   @Prop() label!: string;
+  @Prop() wasted?: boolean;
 }
 </script>
