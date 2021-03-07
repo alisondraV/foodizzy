5dd5a5effa6544c5e76d850988205dc153e3fdf6
alisondraV <alice.7414122013@gmail.com>
2021-03-07 12:23:46 -0500

    Clean up the mixin

--- src/components/AlertMixin.ts
+++ src/components/AlertMixin.ts
@@ -3,7 +3,7 @@ import { Component, Vue } from "vue-property-decorator";
 @Component
 export class AlertMixin extends Vue {
   alertMessage = "";
-  prevTimeout = null;
+  prevTimeout: number | undefined;
 
   showAlert(message: string) {
     clearTimeout(this.prevTimeout);
@@ -12,7 +12,7 @@ export class AlertMixin extends Vue {
       this.alertMessage = message;
 
       this.prevTimeout = setTimeout(() => {
-        resolve(this.alertMessage = "");
+        resolve((this.alertMessage = ""));
       }, 3000);
     });
   }
