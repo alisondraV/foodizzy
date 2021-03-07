332fee994a8cc8358d725b71870982c384003c1a
alisondraV <alice.7414122013@gmail.com>
2021-03-07 12:21:25 -0500

    Clear timeout when calling showAlert multiple times

--- src/components/AlertMixin.ts
+++ src/components/AlertMixin.ts
@@ -3,13 +3,16 @@ import { Component, Vue } from "vue-property-decorator";
 @Component
 export class AlertMixin extends Vue {
   alertMessage = "";
+  prevTimeout = null;
 
   showAlert(message: string) {
+    clearTimeout(this.prevTimeout);
+
     return new Promise(resolve => {
       this.alertMessage = message;
 
-      setTimeout(() => {
-        resolve((this.alertMessage = ""));
+      this.prevTimeout = setTimeout(() => {
+        resolve(this.alertMessage = "");
       }, 3000);
     });
   }
