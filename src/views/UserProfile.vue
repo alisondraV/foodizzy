5cfec33615593d8cbf28ad3570b3afb496b53178
alisondraV <alice.7414122013@gmail.com>
2021-03-08 23:19:16 -0500

    Make MyFamily button optional

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -29,7 +29,11 @@
 
           <hr class="w-full border-secondary-text mb-5" />
 
-          <li class="flex justify-between mb-5" @click="viewFamily">
+          <li
+            v-if="family"
+            class="flex justify-between mb-5"
+            @click="viewFamily"
+          >
             <img src="@/assets/images/Family.svg" alt="Family" />
             <span class="flex-1 text-left ml-4">My Family</span>
             <img src="@/assets/images/Arrow.svg" alt="Go" />
