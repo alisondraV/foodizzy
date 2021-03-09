7e598560a6d2d7666cc55b6daf79f32115d0b47d
alisondraV <alice.7414122013@gmail.com>
2021-03-08 23:10:35 -0500

    Allow editing on ProfileInformation page

--- src/views/profile-settings/PersonalInformation.vue
+++ src/views/profile-settings/PersonalInformation.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <v-header heading="Personal Information" />
-    <div class="mt-24 mb-20 mx-8">
+    <div class="mt-24 mb-20 mx-8 text-primary-text">
       <div v-if="!user">Loading...</div>
       <div v-else class="w-full flex flex-col items-center text-center">
         <img
+          v-if="user.photoURL"
+          alt="profile-image"
           class="mb-4 rounded-full w-1/3"
           :src="user.photoURL"
-          alt="profile-image"
         />
         <p
           class="text-xl place-self-center font-extrabold text-primary-text mb-8"
@@ -15,19 +16,37 @@
           {{ user.displayName }}
         </p>
       </div>
-      <div class="flex justify-between mb-5">
-        <p>Personal Information</p>
-        <p>Edit</p>
+      <div class="flex justify-between mb-4">
+        <p class="text-primary-text">Personal Information</p>
+        <p class="text-dark-peach underline" @click="flipEditMode">
+          {{ editMode ? "Cancel" : "Edit" }}
+        </p>
       </div>
+
       <hr class="w-full border-secondary-text mb-5" />
-      <div>
-        <p>Full Name</p>
-        <p>{{ user.displayName }}</p>
+
+      <div class="mb-3">
+        <p class="text-sm">Full Name</p>
+        <p v-if="!editMode">{{ user.displayName }}</p>
+        <v-input
+          v-else
+          v-model="newName"
+          class="w-full"
+        />
       </div>
       <div>
-        <p>Email</p>
-        <p>{{ user.email }}</p>
+        <p class="text-sm">Email</p>
+        <p v-if="!editMode">{{ user.email }}</p>
+        <v-input
+          v-else
+          v-model="newEmail"
+          class="mb-3 w-full"
+        />
       </div>
+    </div>
+
+    <div v-if="editMode" class="bg-background h-24 w-full bottom-0 fixed">
+      <v-button class="mx-8" label="Save" @click="saveChanges" />
     </div>
   </div>
 </template>
@@ -36,16 +55,36 @@
 import firebase from "firebase";
 import { Component, Vue } from "vue-property-decorator";
 import Authentication from "@/utils/Authentication";
+import VButton from "@/components/VButton.vue";
+import VInput from "@/components/VInput.vue";
 import VHeader from "@/components/VHeader.vue";
 
 @Component({
-  components: { VHeader }
+  components: { VButton, VInput, VHeader }
 })
 export default class PersonalInformation extends Vue {
+  editMode = false;
+  newName: string;
+  newEmail: string;
   user: firebase.User | null = null;
 
   async mounted() {
     this.user = await Authentication.instance.getCurrentUser();
+  }
+
+  async saveChanges() {
+    await Authentication.instance.updateCurrentUser(
+      this.user,
+      this.newName,
+      this.newEmail
+    );
+    this.flipEditMode();
+  }
+
+  flipEditMode() {
+    this.newName = this.user.displayName;
+    this.newEmail = this.user.email;
+    this.editMode = !this.editMode;
   }
 }
 </script>
