8c4238afdc66ba0f93188d88e15d569f12f58180
alisondraV <alice.7414122013@gmail.com>
2021-03-16 16:43:53 -0400

    Remove unused alert on QuitFamily

--- src/views/profile-settings/QuitFamily.vue
+++ src/views/profile-settings/QuitFamily.vue
@@ -1,14 +1,7 @@
 <template>
   <div>
     <v-header heading="Quit My Family" />
-    <div class="mt-20">
-      <v-alert
-        v-if="alertMessage"
-        :isPositive="isPositive"
-        :label="alertMessage"
-      />
-    </div>
-    <div class="mb-20 mx-8" :class="alertMessage ? 'mt-6' : 'mt-24'">
+    <div class="mb-20 mx-8 mt-24">
       <div v-if="!family">Loading...</div>
       <div v-else class="w-full">
         <div class="text-primary-text mb-3">Enter the family name to quit.</div>
@@ -34,24 +27,20 @@
 import { Component, Vue } from "vue-property-decorator";
 import { CurrentFamily } from "@/types";
 import Family from "@/types/Family";
-import VAlert from "@/components/VAlert.vue";
 import VButton from "@/components/VButton.vue";
 import VInput from "@/components/VInput.vue";
 import VHeader from "@/components/VHeader.vue";
 
 @Component({
   components: {
-    VAlert,
     VButton,
     VInput,
     VHeader
   }
 })
 export default class QuitFamily extends Vue {
-  alertMessage = "";
   family: Family | null = null;
   familyNameInputValue = "";
-  isPositive = false;
 
   async mounted() {
     this.family = await CurrentFamily.instance.getCurrentFamily();
