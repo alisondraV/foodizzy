1f510e12c04994bbbf6047209f9469577508ce3b
alisondraV <alice.7414122013@gmail.com>
2021-03-20 10:53:22 -0400

    Update onboarding pages and custom v-button

--- src/views/onboarding/OnboardingMakeLists.vue
+++ src/views/onboarding/OnboardingMakeLists.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <skip-header @click="goToTheFridgeSetup" />
+    <skip-header @click="goToTheCreateFamily" />
     <div class="mt-16 mx-6 flex flex-col items-center text-primary-text text-center">
       <img src="@/assets/images/MakeLists.svg" alt="Shopping Lists" class="mb-10" />
       <h1 class="text-header-onboarding font-extrabold mb-3">
         Make Shopping Lists
       </h1>
-      <p class="mb-6">
+      <p class="mb-8">
         Are you struggling with your grocery shopping experience? Our app will help you to improve your
         shopping habits.
       </p>
-      <img src="@/assets/images/DotsSecond.svg" alt="Shopping Lists" class="mb-8" />
+      <img src="@/assets/images/DotsSecond.svg" alt="Shopping Lists" class="mb-10" />
       <v-button class="w-full" label="Next" @click="goToTheNextOnboarding" />
     </div>
   </div>
@@ -31,8 +31,8 @@ import VInput from '@/components/VInput.vue';
   }
 })
 export default class OnboardingMakeLists extends Vue {
-  goToTheFridgeSetup() {
-    router.safePush('/fridge-setup');
+  goToTheCreateFamily() {
+    router.safePush('/create-family');
   }
 
   goToTheNextOnboarding() {
--- src/views/onboarding/OnboardingTrackWaste.vue
+++ src/views/onboarding/OnboardingTrackWaste.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <skip-header @click="goToTheFridgeSetup" />
+    <skip-header @click="goToTheCreateFamily" />
     <div class="mt-16 mx-6 flex flex-col items-center text-primary-text text-center">
       <img src="@/assets/images/TrackWaste.svg" alt="Track Waste" class="mb-4" />
       <h1 class="text-header-onboarding font-extrabold mb-3">
         Track Your Food Waste
       </h1>
-      <p class="mb-6">
+      <p class="mb-8">
         How much of food waste do you produce every month? We will help you to reduce the amount of food
         waste. Let’s save this planet together!
       </p>
-      <img src="@/assets/images/DotsFirst.svg" alt="Track Waste" class="mb-8" />
+      <img src="@/assets/images/DotsFirst.svg" alt="Track Waste" class="mb-10" />
       <v-button class="w-full" label="Next" @click="goToTheNextOnboarding" />
     </div>
   </div>
@@ -31,8 +31,8 @@ import VInput from '@/components/VInput.vue';
   }
 })
 export default class OnboardingTrackWaste extends Vue {
-  goToTheFridgeSetup() {
-    router.safePush('/fridge-setup');
+  goToTheCreateFamily() {
+    router.safePush('/create-family');
   }
 
   goToTheNextOnboarding() {
