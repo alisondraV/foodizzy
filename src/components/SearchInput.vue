d048ea4c5400fabc7d4c8531db34f015b0619c64
ANDREYDEN <andrey2850@gmail.com>
2021-04-06 21:05:50 -0400

    Add button for uploading a file

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="border border-secondary-text rounded-md flex w-full">
-    <img class="pl-4" src="@/assets/images/Search.svg" alt="Finished" />
-    <input
-      class="text-primary-text h-10 w-full p-4 mr-2 focus:outline-none"
-      :value="value"
-      @focus="focused = true"
-      @blur="focused = false"
-      @input="$emit('input', $event.target.value)"
-      type="text"
-      placeholder="What are you looking for?"
-    />
+  <div class="flex w-full">
+    <div class="flex border border-secondary-text rounded-md">
+      <img class="pl-4" src="@/assets/images/Search.svg" alt="Finished" />
+      <input
+        class="text-primary-text h-10 w-full p-4 mr-2 focus:outline-none"
+        :value="value"
+        @focus="focused = true"
+        @blur="focused = false"
+        @input="$emit('input', $event.target.value)"
+        type="text"
+        placeholder="What are you looking for?"
+      />
+    </div>
+    <div>
+      <img class="absolute ml-3 py-1 w-6 mt-1" src="@/assets/images/Plus.svg" alt="NewProduct" />
+      <input
+        class="opacity-0 ml-3 mt-1 w-6"
+        type="file"
+        accept="image/x-png,image/jpeg,image/gif"
+        @change="e => scanItem(e.target.files)"
+      />
+    </div>
   </div>
 </template>
 
@@ -23,6 +34,10 @@ export default class SearchInput extends Vue {
 
   get isFocused() {
     return this.focused;
+  }
+
+  scanItem(item) {
+    console.log(item);
   }
 }
 </script>
