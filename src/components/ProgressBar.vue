127984365089b4238979eb74c3c46ea958c83bd6
alisondraV <alice.7414122013@gmail.com>
2021-04-07 21:20:41 -0400

    Create custom progress bar

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full">
+    <div class="mb-2 text-primary-text text-sm">{{ percentage }} % of all food waste is {{ category }}.</div>
+    <div class="overflow-hidden h-4 mb-4 text-xs flex rounded-lg bg-light-grey">
+      <div :style="`width: ${percentage}%; background: ${color}`" class="m-progress rounded-lg" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ProgressBar extends Vue {
+  @Prop() category!: string;
+  @Prop() color!: number;
+  @Prop() percentage!: number;
+}
+</script>
