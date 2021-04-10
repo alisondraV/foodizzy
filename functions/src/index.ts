ded2afcd4e69346e00785bad89c3a11eeb38e764
Andrii Denysenko <andrey2850@gmail.com>
2021-04-10 19:22:25 -0400

    Object Detection (#137)
    * Add button for uploading a file
    
    * wip: add food classifier
    
    * Make image detection work even slightly
    
    * Clean up unused code
    
    * Add compression
    
    * Add new on-device approach
    
    * Extract ML functionality into a utility
    
    * Add a proper icon
    
    * Clean up dependencies
    
    * Only allow object detection on the fridge page
    
    * Update tsconfig

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,92 +1,84 @@
 import * as functions from 'firebase-functions';
 import sendEmail from './sendEmail';
 import axios from 'axios';
-import {db, auth} from './admin';
+import { db, auth } from './admin';
 
 export const onFamilyUpdate = functions.firestore
-    .document('/family/{familyId}')
-    .onUpdate(async (change, context) => {
-      const oldFamily = change.before.data();
-      const newFamily = change.after.data();
+  .document('/family/{familyId}')
+  .onUpdate(async (change, context) => {
+    const oldFamily = change.before.data();
+    const newFamily = change.after.data();
 
-      const newStorage = newFamily.storage;
-      const oldStorage = oldFamily.storage;
-      if (newStorage.length > oldStorage.length) {
-        return updateTotalProducts(newFamily, oldFamily, change);
-      }
+    const newStorage = newFamily.storage;
+    const oldStorage = oldFamily.storage;
+    if (newStorage.length > oldStorage.length) {
+      return updateTotalProducts(newFamily, oldFamily, change);
+    }
 
-      const newPendingMembers = newFamily.pendingMembers ?? [];
-      const oldPendingMembers = oldFamily.pendingMembers ?? [];
-      if (newPendingMembers.length > oldPendingMembers.length) {
-        return sendWelcomeEmails(newFamily, oldFamily);
-      }
+    const newPendingMembers = newFamily.pendingMembers ?? [];
+    const oldPendingMembers = oldFamily.pendingMembers ?? [];
+    if (newPendingMembers.length > oldPendingMembers.length) {
+      return sendWelcomeEmails(newFamily, oldFamily);
+    }
 
-      return checkForFamilyRemoval(change.after);
-    });
+    return checkForFamilyRemoval(change.after);
+  });
 
 export const onFamilyCreate = functions.firestore
-    .document('/family/{familyId}')
-    .onCreate((snapshot, context) => {
-      const newFamily = snapshot.data();
+  .document('/family/{familyId}')
+  .onCreate((snapshot, context) => {
+    const newFamily = snapshot.data();
 
-      return sendWelcomeEmails(newFamily);
-    });
+    return sendWelcomeEmails(newFamily);
+  });
 
 export const onFamilyDelete = functions.firestore
-    .document('/family/{familyId}')
-    .onDelete(async (snapshot, context) => {
-      const wasteBucketQueryResults = await db
-          .collection('wasteBuckets')
-          .where('familyId', '==', snapshot.id)
-          .get();
+  .document('/family/{familyId}')
+  .onDelete(async (snapshot, context) => {
+    const wasteBucketQueryResults = await db
+      .collection('wasteBuckets')
+      .where('familyId', '==', snapshot.id)
+      .get();
 
-      return Promise.all(
-          wasteBucketQueryResults.docs.map((doc) => doc.ref.delete())
-      );
-    });
+    return Promise.all(wasteBucketQueryResults.docs.map(doc => doc.ref.delete()));
+  });
 
-export const syncEmulatorAllProducts = functions.https.onCall(
-    async (data, context) => {
-      const allProductsRef = db.collection('allProducts');
-      const allProductsSnap = await allProductsRef.get();
-      await Promise.all(
-          (await allProductsSnap).docs.map((doc) => doc.ref.delete())
-      );
+export const syncEmulatorAllProducts = functions.https.onCall(async (data, context) => {
+  const allProductsRef = db.collection('allProducts');
+  const allProductsSnap = await allProductsRef.get();
+  await Promise.all((await allProductsSnap).docs.map(doc => doc.ref.delete()));
 
-      return Promise.all(
-          data.products.map((product: FirebaseFirestore.DocumentData) => {
-            const id = product.id;
-            delete product.id;
-            return db.doc(`allProducts/${id}`).set(product);
-          })
-      );
-    }
-);
+  return Promise.all(
+    data.products.map((product: FirebaseFirestore.DocumentData) => {
+      const id = product.id;
+      delete product.id;
+      return db.doc(`allProducts/${id}`).set(product);
+    })
+  );
+});
 
 export const getUsersByEmail = functions.https.onCall((data, context) => {
   if (!data.emails) {
     return [];
   }
-  return Promise.all(
-      data.emails.map((email: string) => auth.getUserByEmail(email))
-  );
+  return Promise.all(data.emails.map((email: string) => auth.getUserByEmail(email)));
 });
 
 async function updateTotalProducts(
-    newFamily: FirebaseFirestore.DocumentData,
-    oldFamily: FirebaseFirestore.DocumentData,
-    change: functions.Change<functions.firestore.QueryDocumentSnapshot>
+  newFamily: FirebaseFirestore.DocumentData,
+  oldFamily: FirebaseFirestore.DocumentData,
+  change: functions.Change<functions.firestore.QueryDocumentSnapshot>
 ) {
   let addedProducts: any[] = newFamily.storage.filter((newProduct: any) => {
     return !oldFamily.storage.find((oldProduct: any) => newProduct.name === oldProduct.name);
   });
-  
+
   if (addedProducts.length === 0) {
     throw new Error('The storages were the same');
   }
 
   console.log('New products were added: ', addedProducts);
-  
+
   const updatedFamilyStats = change.after.ref.collection('statistics');
 
   const thisMonthStatsDoc = await getThisMonthStats(updatedFamilyStats);
@@ -94,7 +86,7 @@ async function updateTotalProducts(
 
   for (const addedProduct of addedProducts) {
     const categoryName = addedProduct!.category.toLowerCase() ?? 'general';
-  
+
     if (!Object.keys(thisMonthData.totalProducts).includes(categoryName)) {
       thisMonthData.totalProducts[categoryName] = 0;
     }
@@ -103,22 +95,22 @@ async function updateTotalProducts(
   }
 
   return await updatedFamilyStats
-      .doc(thisMonthStatsDoc.id)
-      .update('totalProducts', thisMonthData.totalProducts);
+    .doc(thisMonthStatsDoc.id)
+    .update('totalProducts', thisMonthData.totalProducts);
 }
 
 async function getThisMonthStats(statsCollection: FirebaseFirestore.CollectionReference) {
   const thisMonthStatsCollection = await statsCollection
-      .where('month', '==', new Date().getMonth())
-      .where('year', '==', new Date().getFullYear())
-      .get();
+    .where('month', '==', new Date().getMonth())
+    .where('year', '==', new Date().getFullYear())
+    .get();
 
   let thisMonthStatsDocRef;
   if (thisMonthStatsCollection.docs.length === 0) {
     thisMonthStatsDocRef = await statsCollection.add({
       month: new Date().getMonth(),
       year: new Date().getFullYear(),
-      totalProducts: {},
+      totalProducts: {}
     });
   } else {
     thisMonthStatsDocRef = thisMonthStatsCollection.docs[0].ref;
@@ -127,8 +119,8 @@ async function getThisMonthStats(statsCollection: FirebaseFirestore.CollectionRe
 }
 
 async function sendWelcomeEmails(
-    newFamily: FirebaseFirestore.DocumentData,
-    oldFamily?: FirebaseFirestore.DocumentData
+  newFamily: FirebaseFirestore.DocumentData,
+  oldFamily?: FirebaseFirestore.DocumentData
 ) {
   const oldMembers = oldFamily?.pendingMembers ?? [];
   const newMembers = newFamily?.pendingMembers ?? [];
@@ -147,15 +139,15 @@ async function sendWelcomeEmails(
   emailTemplate = emailTemplate.replace('{FAMILY NAME}', `"${newFamily.name}"`);
 
   return Promise.all(
-      newEmails.map((email: string) => {
-        return sendEmail({
-          to: [email],
-          message: {
-            subject: 'Welcome to Foodizzy!',
-            html: emailTemplate,
-          },
-        });
-      })
+    newEmails.map((email: string) => {
+      return sendEmail({
+        to: [email],
+        message: {
+          subject: 'Welcome to Foodizzy!',
+          html: emailTemplate
+        }
+      });
+    })
   );
 }
 
@@ -163,7 +155,7 @@ async function checkForFamilyRemoval(newDoc: FirebaseFirestore.QueryDocumentSnap
   const newFamily = newDoc.data();
   if (newFamily.members?.length === 0) {
     const statisticsRefs = await newDoc.ref.collection('statistics').get();
-    await Promise.all(statisticsRefs.docs.map((doc) => doc.ref.delete()));
+    await Promise.all(statisticsRefs.docs.map(doc => doc.ref.delete()));
     return newDoc.ref.delete();
   }
   return null;
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,28 +1,64 @@
 <template>
-  <div class="border border-secondary-text rounded-md flex w-full">
-    <img class="pl-4" src="@/assets/images/Search.svg" alt="Finished" />
-    <input
-      class="text-primary-text h-10 w-full p-4 mr-2 focus:outline-none"
-      :value="value"
-      @focus="focused = true"
-      @blur="focused = false"
-      @input="$emit('input', $event.target.value)"
-      type="text"
-      placeholder="What are you looking for?"
-    />
+  <div class="flex w-full">
+    <div class="flex border border-secondary-text rounded-md">
+      <img class="pl-4" src="@/assets/images/Search.svg" alt="Finished" />
+      <input
+        class="text-primary-text h-10 w-full p-4 mr-2 focus:outline-none"
+        :value="value"
+        @focus="focused = true"
+        @blur="focused = false"
+        @input="$emit('input', $event.target.value)"
+        type="text"
+        placeholder="What are you looking for?"
+      />
+    </div>
+    <div v-if="currentPage == 'Fridge'">
+      <img class="absolute ml-3 py-1 w-6 mt-1" src="@/assets/images/TakePhoto.svg" alt="NewProduct" />
+      <input
+        class="opacity-0 ml-3 mt-1 w-6"
+        type="file"
+        accept="image/x-png,image/jpeg,image/gif"
+        @change="e => scanItem(e.target.files)"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import Firestore from '@/utils/Firestore';
+import { Component, Inject, Prop, Vue } from 'vue-property-decorator';
+import ML from '@/utils/ML';
 
 @Component
 export default class SearchInput extends Vue {
   @Prop() value!: null;
+  @Inject('currentPage') currentPage!: string;
   focused = false;
 
   get isFocused() {
     return this.focused;
+  }
+
+  async scanItem(files: File[]): Promise<void> {
+    try {
+      const file = files[0];
+      const image = await ML.toImage(file);
+
+      const predictionTensor = await ML.predict(image);
+      const topPredictions = await ML.getTopKResults(predictionTensor, 3);
+      console.log(topPredictions);
+
+      await Firestore.instance.addToList(
+        [
+          {
+            name: topPredictions[0].className
+          }
+        ],
+        'storage'
+      );
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
 </script>
