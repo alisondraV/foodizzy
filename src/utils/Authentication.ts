3921cf7b4c7183165b1645096ba92ebfe576accf
Alisa Vynohradova <56138100+alisondraV@users.noreply.github.com>
2021-04-10 21:11:09 -0400

    Simplify function
    Co-authored-by: Andrii Denysenko <andrey2850@gmail.com>

--- src/utils/Authentication.ts
+++ src/utils/Authentication.ts
@@ -33,7 +33,7 @@ export default class Authentication {
   }
 
   public async getFirstName(user?: firebase.User): Promise<string> {
-    const neededUser = user ? user : await this.getCurrentUser();
+    const neededUser = user ?? await this.getCurrentUser();
     const name = neededUser?.displayName ?? '';
     return name.substr(0, name?.indexOf(' ')) || name;
   }
