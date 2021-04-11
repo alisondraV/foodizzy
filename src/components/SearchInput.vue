3b63cf6d1e71bd240a8e9fc0d7e229714ed98c27
Alisa Vynohradova <56138100+alisondraV@users.noreply.github.com>
2021-04-11 13:00:46 -0400

    Fix styling and image for TakePhoto (#140)

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex w-full">
-    <div class="flex border border-secondary-text rounded-md">
-      <img class="pl-4" src="@/assets/images/Search.svg" alt="Finished" />
+    <div class="flex flex-1 border border-secondary-text rounded-md mr-1">
+      <img class="pl-3" src="@/assets/images/Search.svg" alt="Finished" />
       <input
-        class="text-primary-text h-10 w-full p-4 mr-2 focus:outline-none"
+        class="text-primary-text text-sm h-10 w-full pl-2 mr-1 focus:outline-none"
         :value="value"
         @focus="focused = true"
         @blur="focused = false"
@@ -12,10 +12,10 @@
         placeholder="What are you looking for?"
       />
     </div>
-    <div v-if="currentPage == 'Fridge'">
-      <img class="absolute ml-3 py-1 w-6 mt-1" src="@/assets/images/TakePhoto.svg" alt="NewProduct" />
+    <div v-if="currentPage === 'Fridge'">
+      <img class="absolute ml-2 py-1" src="@/assets/images/TakePhoto.svg" alt="NewProduct" />
       <input
-        class="opacity-0 ml-3 mt-1 w-6"
+        class="opacity-0 ml-2 mt-1 w-10"
         type="file"
         accept="image/x-png,image/jpeg,image/gif"
         @change="e => scanItem(e.target.files)"
