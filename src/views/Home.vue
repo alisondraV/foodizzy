b13e74c809241423b73969bc578c4b3aee0b4f43
alisondraV <alice.7414122013@gmail.com>
2021-04-11 19:16:16 -0400

    Fix reactivity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,7 +30,7 @@
         </div>
         <div v-else>
           <!--for reactivity-->
-          <div v-for="chart in chartData" :key="chart[0]">
+          <div v-for="chart in chartData" :key="chart">
             <custom-chart
               v-if="chart.length !== 0"
               class="mb-6"
