539ba6d17c911f2f085caa7b50534247c71b9ef8
ANDREYDEN <andrey2850@gmail.com>
2021-04-23 23:05:31 -0400

    Fix product tests

--- cypress/integration/products.spec.js
+++ cypress/integration/products.spec.js
@@ -1,13 +1,41 @@
 import productsData from '../fixtures/products.json';
+import user from '../fixtures/user.json';
 
 describe('Products', () => {
   const products = productsData.products;
 
-  it('test', () => {
-    cy.visit('localhost:8080/fridge-setup');
+  beforeEach(() => {
+    cy.request(
+      'DELETE',
+      'http://localhost:8888/emulator/v1/projects/foodizzy-app/databases/(default)/documents'
+    );
+
+    cy.request('DELETE', 'http://localhost:9099/emulator/v1/projects/foodizzy-app/accounts');
+
+    products.forEach(product => {
+      cy.callFirestore('set', `allProducts/${product.name}`, product);
+    });
+
+    cy.request('POST', 'http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signUp?key=foo', {
+      email: user.email,
+      password: user.password
+    });
+
+    cy.callFirestore('set', 'family/MyFamily', {
+      members: [user.email],
+      pendingMembers: [],
+      storage: products,
+      shoppingList: []
+    });
+
+    cy.visit('localhost:8080/sign-in');
+
+    cy.get('[data-cy=email]').type(user.email);
+    cy.get('[data-cy=password]').type(user.password);
+    cy.get('[data-cy=sign-in]').click();
   });
 
-  it.only('products can be consumed and move to the shopping list', () => {
+  it.only('products can be consumed and moved to the shopping list', () => {
     cy.visit('localhost:8080/fridge');
 
     cy.contains(products[0].category)
@@ -17,6 +45,10 @@ describe('Products', () => {
       .first()
       .click();
     cy.get('[data-cy=consume]').click();
+
+    // wait for product to be added to the shopping list
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000);
 
     cy.visit('localhost:8080/shopping-list');
     cy.contains(products[0].category)
