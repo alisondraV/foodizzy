f7addd9f54dd30bead88336afd6d09853cec6cfd
alisondraV <alice.7414122013@gmail.com>
2022-05-23 16:17:51 -0400

    Re-use EmailsList component on the CreateFamily page

--- src/views/CreateFamily.vue
+++ src/views/CreateFamily.vue
@@ -6,56 +6,23 @@
       </p>
       <div class="flex-start">
         <v-input
-          class="mb-6"
+          class="mb-2"
           data-cy="name"
           type="text"
           label="Family Name"
           placeholder="Enter your family name"
           v-model="familyName"
         />
+        <div class="text-dark-peach mb-6">{{ errorMessage }}</div>
         <p class="text-1xl font-bold text-primary-text mb-4">
           Invite Family Members
         </p>
-        <div class="flex flex-row justify-between mb-4">
-          <v-input
-            class="w-5/6"
-            data-cy="member-email"
-            type="email"
-            label="Email Address"
-            placeholder="Enter member's email"
-            v-model="currentEmail"
-            :error="errorType === 'email'"
-          />
-          <img
-            alt="Add"
-            class="mt-6"
-            data-cy="add-member"
-            src="@/assets/images/PlusIcon.svg"
-            v-if="isEmailInValidState"
-            @click="addEmail"
-          />
-        </div>
-        <ul>
-          <li
-            v-for="email in memberEmails"
-            :key="email"
-            class="flex justify-between items-center bg-light-yellow py-2 px-4 rounded text-sm mb-2"
-          >
-            <p>{{ email }}</p>
-            <img
-              alt="remove"
-              class="w-4 h-4 text-primary-text"
-              src="@/assets/images/Cross.svg"
-              @click="removeEmail(email)"
-            />
-          </li>
-        </ul>
+        <emails-list :member-emails="memberEmails" />
       </div>
     </div>
     <div class="bg-background h-24 w-full bottom-0 fixed">
-      <div class="text-dark-peach mx-8">{{ errorMessage }}</div>
       <v-button
-        class="mx-8 mt-3"
+        class="mx-8"
         data-cy="create"
         label="Create Family"
         :disabled="validationFailed"
@@ -72,9 +39,11 @@ import { CurrentFamily } from '@/types';
 import { PathName } from '@/utils/enums';
 import { ValidationMixin } from '@/mixins';
 import router from '@/router';
+import EmailsList from '@/components/EmailsList.vue';
 
 @Component({
   components: {
+    EmailsList,
     VButton,
     VInput
   }
@@ -82,7 +51,6 @@ import router from '@/router';
 export default class CreateFamily extends Mixins(ValidationMixin) {
   familyName = '';
   memberEmails: string[] = [];
-  currentEmail = '';
 
   async createFamily() {
     await CurrentFamily.instance.create(this.familyName, this.memberEmails);
@@ -93,24 +61,8 @@ export default class CreateFamily extends Mixins(ValidationMixin) {
     return this.isDisplayNameValid(this.familyName);
   }
 
-  get isEmailInValidState() {
-    this.errorMessage = '';
-    this.errorType = '';
-
-    return this.currentEmail !== '' && this.isEmailValid(this.currentEmail);
-  }
-
-  addEmail() {
-    this.memberEmails.push(this.currentEmail);
-    this.currentEmail = '';
-  }
-
   goToTheNextPage() {
     router.safePush!(PathName.StorageSetup);
-  }
-
-  removeEmail(email: string) {
-    this.memberEmails.splice(this.memberEmails.indexOf(email), 1);
   }
 }
 </script>
