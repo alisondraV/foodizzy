be883a68a3a6edc0f15b1503c4421d63c71b4c2f
Vehikl Morning Mob <39924126+vehikl-morning-mob@users.noreply.github.com>
2022-06-01 16:47:03 -0400

    Add functions folder linting to the CI (#178)
    * Add functions folder linting to the CI
    
    * Add command to install dependencies for functions folder
    
    * Fix linting errors
    
    * Merge emulating and re-building functions
    
    Co-authored-by: alisondraV <alice.7414122013@gmail.com>

--- functions/src/utils/sendEmail.ts
+++ functions/src/utils/sendEmail.ts
@@ -29,8 +29,8 @@ export async function sendWelcomeEmails(
 
     console.log('New Members: ', newEmails);
 
-    const htmlURL =
-    'https://firebasestorage.googleapis.com/v0/b/foodizzy-app.appspot.com/o/Foodizzy.html?alt=media&token=53d69dc9-3b9a-42fc-a6d8-9a89efdcc26b';
+    const htmlURL = 'https://firebasestorage.googleapis.com/v0/b/foodizzy-app.appspot.com/o/Foodizzy.html?' +
+        'alt=media&token=53d69dc9-3b9a-42fc-a6d8-9a89efdcc26b';
     const response = await axios.get(htmlURL);
     let emailTemplate = response.data;
 
